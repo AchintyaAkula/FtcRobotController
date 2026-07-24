b192c079726082eb6ad12aa0deda50df94a97b90
AchintyaAkula <achintya.akula@gmail.com>
2026-07-24 10:02:15 -0700

    Setting up spotless apply

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/InstantCommand.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/InstantCommand.kt
@@ -2,7 +2,9 @@ package org.firstinspires.ftc.teamcode.commands
 
 import com.pedropathing.ivy.CommandBuilder
 
-open class InstantCommand(task: Runnable) : CommandBuilder() {
+open class InstantCommand(
+    task: Runnable,
+) : CommandBuilder() {
     init {
         setStart(task)
         setDone { true }
